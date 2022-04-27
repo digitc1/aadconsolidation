1a3040fba6f816583ef72f9eb5669a325f7b3031
lashegu <86957383+lashegu@users.noreply.github.com>
2022-04-27 09:46:48 +0200

    Update set-tenant.ps1

--- src/target/set-tenant.ps1
+++ src/target/set-tenant.ps1
@@ -9,8 +9,8 @@ ForEach($user in $userList){
 		if(Get-AzADUser -Mail $user.mail){
            		Write-Host "User already added in the consolidated tenant"
        		} else {
-			Write-Host "Inviting user $user.mail to the consolidated tenant"
-            		az rest --method POST --uri "https://graph.microsoft.com/v1.0/invitations" --body "{\""invitedUserEmailAddress\"": \""$user.mail\"", \""inviteRedirectUrl\"": \""http://myapps.microsoft.com\""}" --headers '{\"Content-Type\":\"application/json\"}'
+			Write-Host "Inviting user $($user.mail) to the consolidated tenant"
+            		az rest --method POST --uri "https://graph.microsoft.com/v1.0/invitations" --body "{\""invitedUserEmailAddress\"": \""$($user.mail)\"", \""inviteRedirectUrl\"": \""http://myapps.microsoft.com\""}" --headers '{\"Content-Type\":\"application/json\"}'
         	}
 	} catch {
             Write-host "Following error was encountered: " $error[0].Exception.ErrorContent.Message.value -ForegroundColor Red
